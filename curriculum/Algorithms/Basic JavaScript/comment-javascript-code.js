f2dc4a38301ea1d822357057e099197cae37a731
ryanmaddox <maddoxryan15@gmail.com>
2020-07-28 03:57:10 +0000

    added description at top of page in comments

--- curriculum/Algorithms/Basic JavaScript/comment-javascript-code.js
+++ curriculum/Algorithms/Basic JavaScript/comment-javascript-code.js
@@ -1,5 +1,7 @@
 /*
+	Basic JavaScript: How to write Comments
 
+	The ability to write comments allows you to leave notes for yourself and for other developers in your code, this will allow you to explain anything you need to understand when looking at your code.
 */
 
 // this is an inline or single line comment
